add30af3e3eee4f2b65a6ce518bba1264cb928c5
jlarata <ariel@cfee.gob.ar>
2025-10-24 13:23:19 -0300

    some more styling to profile and buttons

--- hunTech/src/app/mock-usuarios.ts
+++ hunTech/src/app/mock-usuarios.ts
@@ -3,7 +3,12 @@ import { Gerente } from "./models/users/gerente";
 import { InstitucionEducativa } from "./models/users/institucion-educativa";
 
 export const DESARROLLADORES: Desarrollador[] = [
-        {id: '4', email: 'devBack@gmail.com', descripcion: "Estudio hace 2 años en el IFTS11 y quiero dominar el mundo", nombre: 'Mati', apellido: 'Melgarejo', fecha_nacimiento: '2001-12-20', rol: 'back end', skills: ['java', 'springboot', 'C#', 'agile'], created_at: '2025-10-22' },
+        {id: '4', email: 'devBack@gmail.com', descripcion: `Estudio hace 2 años en el IFTS11 y quiero dominar el mundo
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora cum voluptates dignissimos adipisci magni eaque, amet alias quas voluptatem soluta! Rerum est sit quisquam dignissimos quas eveniet iusto ab exercitationem.
+Sequi obcaecati aliquam, eum itaque ad corrupti nulla quidem? Quis ipsam voluptatibus quibusdam! Fuga dicta quo culpa accusantium, corrupti, sequi illum sed, obcaecati saepe nisi alias rem! Enim, itaque nulla.
+Voluptatum nesciunt accusamus maxime molestiae dolore corporis, aut ex nemo nihil cupiditate maiores necessitatibus eligendi libero iste perferendis quidem? Odit cupiditate recusandae voluptas non sequi repellendus velit inventore minus maiores?
+Placeat, quasi odit magni sapiente reiciendis dignissimos incidunt quae nisi ea ex nemo laboriosam suscipit molestiae veniam odio deserunt! Cumque repellendus blanditiis non asperiores vitae voluptatem, dolore iure labore dicta!
+Explicabo voluptatibus tempora, debitis sint minima provident consectetur quisquam repudiandae doloribus quaerat tenetur architecto cupiditate? Autem velit provident magni itaque pariatur, facere asperiores nam explicabo doloribus voluptatem consequatur incidunt nihil?`, nombre: 'Mati', apellido: 'Melgarejo', fecha_nacimiento: '2001-12-20', rol: 'back end', skills: ['java', 'springboot', 'C#', 'agile'], created_at: '2025-10-22' },
         {id: '5', email: 'devFront@gmail.com', descripcion: "ME caí en el baño e inventé el condensador de flujo", nombre: 'Nahuel', apellido: 'Castro', fecha_nacimiento: '2001-12-21', rol: 'front end', skills: ['javascript', 'angular', 'scrum'], created_at: '2025-10-22' },
     ]
 export const GERENTES: Gerente[] = [
